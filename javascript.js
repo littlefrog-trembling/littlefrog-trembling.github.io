9a8113bf7d6d6d0aff63f20b743690739ace98c4
littlefrog-trembling <46259510+littlefrog-trembling@users.noreply.github.com>
2019-04-25 23:07:32 +1000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,13 @@
 var quotes = [
-  'Dream. I ask, "do you think I'm in trouble?" Then she says, "right now... maybe. Cosmically? Definitely." We stand there a while. She looks at me, "I'll see what I can do."'
-  'was there scaffolding around the Tower of Babel?'
-  'folding into the body of night'
+  'Dream. I ask, "do you think Im in trouble?"" Then she says, "right now, ...maybe. Cosmically? Definitely." So we stand there a while. She looks at me and says "Ill see what I can do."'
+  'was there scaffolding around the Tower of Bable?'
   'a man clapping passionately. tear streams down.'
   'phantom bread'
-  'fudgsicle eating boss'
+  'fudsicle-eating boss'
   'goals: make sexy anthem.'
   'paypal $80'
-]
+  ]
 function newQuote() {
-  var randomNumber = Math.floor(Math.random() * (quotes.length)); 
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
   document.getElementById('quoteDisplay') .innerHTML = quotes[randomNumber];
 }
